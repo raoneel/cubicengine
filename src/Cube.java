98628c25cea02ff584f284366a6970fd0b4830cb
Neel <raoneel@gmail.com>
2013-04-18 21:51:28 -0700

    added lighting, cleaned up code

--- src/Cube.java
+++ src/Cube.java
@@ -16,84 +16,56 @@ public class Cube {
 		pos.z = z;
 		this.width = width;
 		
+		// Give the cube a random color for now
 		this.color.x = (float) Math.random();
 		this.color.y = (float) Math.random();
 		this.color.z = (float) Math.random();
 	}
 	
 	public void draw() {
-	    //bottom
+	    
 		GL11.glColor3f(color.x,color.y,color.z);
 		GL11.glColorMaterial(GL11.GL_FRONT, GL11.GL_DIFFUSE);
-		Vector3f edge1 = new Vector3f();
-		Vector3f edge2 = new Vector3f();
-		Vector3f normal = new Vector3f();
 		
-		edge1.x = -width;
-		edge1.y = 0;
-		edge1.z = 0;
-		edge2.x = 0;
-		edge2.y = -width;
-		edge2.z = 0;
-		Vector3f.cross(edge1, edge2, normal);
-		normal.normalise();
-		GL11.glNormal3f(normal.x, normal.y, normal.z);
-		
+		//front
+		GL11.glNormal3f(0, 0, 1);
 	    GL11.glVertex3f(pos.x, pos.y, pos.z);
 	    GL11.glVertex3f(pos.x + width, pos.y, pos.z);
 	    GL11.glVertex3f(pos.x + width, pos.y - width, pos.z);
 	    GL11.glVertex3f(pos.x, pos.y - width, pos.z);
-	    normal.normalise();
-		GL11.glNormal3f(-normal.x, -normal.y, -normal.z);
-		
-	    //top
-	    GL11.glVertex3f(pos.x, pos.y, pos.z + width);
-	    GL11.glVertex3f(pos.x + width, pos.y, pos.z + width);
-	    GL11.glVertex3f(pos.x + width, pos.y - width, pos.z + width);
-	    GL11.glVertex3f(pos.x, pos.y - width, pos.z + width);
-	    
-		edge1.x = 0;
-		edge1.y = 0;
-		edge1.z = -width;
-		edge2.x = width;
-		edge2.y = 0;
-		edge2.z = 0;
-		Vector3f.cross(edge1, edge2, normal);
-		normal.normalise();
-		GL11.glNormal3f(normal.x, normal.y, normal.z);
-	    
-	    //front
-	    GL11.glVertex3f(pos.x, pos.y - width, pos.z);
-	    GL11.glVertex3f(pos.x, pos.y - width, pos.z + width);
-	    GL11.glVertex3f(pos.x + width, pos.y - width, pos.z + width);
-	    GL11.glVertex3f(pos.x + width, pos.y - width, pos.z);
-	    
-	    normal.normalise();
-	    GL11.glNormal3f(-normal.x, -normal.y, -normal.z);
+
+		GL11.glNormal3f(0, 0, -1);
 	    //back
+	    GL11.glVertex3f(pos.x, pos.y, pos.z + width);
+	    GL11.glVertex3f(pos.x + width, pos.y, pos.z + width);
+	    GL11.glVertex3f(pos.x + width, pos.y - width, pos.z + width);
+	    GL11.glVertex3f(pos.x, pos.y - width, pos.z + width);
+	    
+
+		GL11.glNormal3f(0, -1, 0);
+	    //bottom
+	    GL11.glVertex3f(pos.x, pos.y - width, pos.z);
+	    GL11.glVertex3f(pos.x, pos.y - width, pos.z + width);
+	    GL11.glVertex3f(pos.x + width, pos.y - width, pos.z + width);
+	    GL11.glVertex3f(pos.x + width, pos.y - width, pos.z);
+	    
+	    
+	    GL11.glNormal3f(0, 1, 0);
+	    //top
 	    GL11.glVertex3f(pos.x, pos.y, pos.z);
 	    GL11.glVertex3f(pos.x, pos.y, pos.z + width);
 	    GL11.glVertex3f(pos.x + width, pos.y, pos.z + width);
 	    GL11.glVertex3f(pos.x + width, pos.y, pos.z);
 	    
-		edge1.x = 0;
-		edge1.y = 0;
-		edge1.z = -width;
-		edge2.x = 0;
-		edge2.y = -width;
-		edge2.z = 0;
-		Vector3f.cross(edge1, edge2, normal);
-		normal.normalise();
-		GL11.glNormal3f(normal.x, normal.y, normal.z);
-	    
+
+	    GL11.glNormal3f(0, 1, 0);
 	    //left
 	    GL11.glVertex3f(pos.x, pos.y, pos.z);
 	    GL11.glVertex3f(pos.x, pos.y, pos.z + width);
 	    GL11.glVertex3f(pos.x, pos.y - width, pos.z + width);
 	    GL11.glVertex3f(pos.x, pos.y - width, pos.z);
 	    
-	    normal.normalise();
-	    GL11.glNormal3f(-normal.x, -normal.y, -normal.z);
+	    GL11.glNormal3f(0, -1, 0);
 	    //right
 	    GL11.glVertex3f(pos.x + width, pos.y, pos.z);
 	    GL11.glVertex3f(pos.x + width, pos.y, pos.z + width);
--- src/Player.java
+++ src/Player.java
@@ -1,15 +1,124 @@
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.util.glu.GLU;
 import org.lwjgl.util.vector.Vector3f;
-
+/**
+ * This class controls the first-person camera and also the keypress actions for the player
+ * @author neelrao
+ *
+ */
 public class Player {
 	public Vector3f camera;
 	public Vector3f lookAt;
+	public Vector3f up;
+	
 	public boolean isFly;
 	
     float mouseSensitivity = 2.0f;
     float movementSpeed = 5000.0f; //move 10 units per second
     
+    public Player() {
+    	camera = new Vector3f(0, 600, 500);
+		lookAt = new Vector3f(0, 600, 0);
+		up = new Vector3f(0, 1, 0);
+		isFly = true;
+    }
+    
+    private void processInput() {
+        
+
+        
+//      while (Keyboard.next()) {
+//  	    if (Keyboard.getEventKeyState()) {
+//  	        if (Keyboard.getEventKey() == Keyboard.KEY_A) {
+//  		    System.out.println("A Key Pressed");
+//  		    cameraX -= 10f;
+//  		}
+//  		if (Keyboard.getEventKey() == Keyboard.KEY_S) {
+//  		    System.out.println("S Key Pressed");
+//  		}
+//  		if (Keyboard.getEventKey() == Keyboard.KEY_D) {
+//  			cameraX += 10f;
+//  		    
+//  		    System.out.println("D Key Pressed");
+//  		}
+//  	    } else {
+//  	        if (Keyboard.getEventKey() == Keyboard.KEY_A) {
+//  		    System.out.println("A Key Released");
+//  	        }
+//  	    	if (Keyboard.getEventKey() == Keyboard.KEY_S) {
+//  		    System.out.println("S Key Released");
+//  		}
+//  		if (Keyboard.getEventKey() == Keyboard.KEY_D) {
+//  		    System.out.println("D Key Released");
+//  		}
+//  	    }
+//  	}
+    }
+    
     public void update(int delta) {
+    	//distance in mouse movement from the last getDX() call.
+        int dx = Mouse.getDX();
+        //distance in mouse movement from the last getDY() call.
+        int dy = Mouse.getDY();
+        
+       
+        lookAt.y += dy * mouseSensitivity;
+        
+        Vector3f view = new Vector3f();
+        Vector3f.sub(lookAt, camera, view);
+        view.normalise();
+        
+        Vector3f right = new Vector3f(0, 0, 0);
+        Vector3f.cross(view, up, right);
+        
+        lookAt.x += right.x * dx * mouseSensitivity;
+        lookAt.z += right.z * dx * mouseSensitivity;
+	
+        float deltaT = delta / 1000.0f;
+        right.scale(movementSpeed * deltaT);
+        
+        if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)) {
+        	movementSpeed = 50000f;
+        }
+        else {
+        	movementSpeed = 5000;
+        }
+        
+        if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
+            lookAt.y += 500.0f;
+            camera.y += 500.0f;
+        }
+        
+        if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
+        	Vector3f.sub(lookAt, right, lookAt);
+        	Vector3f.sub(camera, right, camera);
+        }
+        
+        if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
+        	Vector3f.add(lookAt, right, lookAt);
+        	Vector3f.add(camera, right, camera);
+        }
+        
+        view.scale(movementSpeed * deltaT);
+        // Flying
+        if (!isFly) {
+        	view.y = 0;
+        }
+        
+        if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
+        	Vector3f.add(lookAt, view, lookAt);
+        	Vector3f.add(camera, view, camera);
+        }
+        
+        if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
+        	Vector3f.sub(lookAt, view, lookAt);
+        	Vector3f.sub(camera, view, camera);
+        }
+        
+        GL11.glLoadIdentity();
+    	GLU.gluLookAt(camera.x, camera.y, camera.z, lookAt.x, lookAt.y, lookAt.z, up.x, up.y, up.z);
     	
     }
 }
